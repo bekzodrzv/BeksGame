d1d3376a234f315b159ba73f3e688d5dcf372402
bekzodrzv <bekzod.ruziev@gmail.com>
2026-02-05 12:10:45 +0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCjb_SxZGmzVQdrY9bCceyEP4jYVTAq8Ps",
@@ -11,24 +11,11 @@ const firebaseConfig = {
     messagingSenderId: "101120658989",
     appId: "1:101120658989:web:513fd1ef29a003605a72a4",
     measurementId: "G-TJHYR914SQ"
-};
+  };
+  
+
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// 🔹 Firestore offline persistence
-try {
-  enableIndexedDbPersistence(db)
-    .then(() => console.log("✅ Firestore offline persistence yoqildi"))
-    .catch((err) => {
-      if (err.code === 'failed-precondition') {
-        console.warn("⚠️ Firestore persistence faqat 1 tabda ishlaydi");
-      } else if (err.code === 'unimplemented') {
-        console.warn("⚠️ Brauzeringiz Firestore persistenceni qo‘llab-quvvatlamaydi");
-      }
-    });
-} catch (e) {
-  console.error("Firestore persistence xato:", e);
-}
